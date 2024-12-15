568b0a3d25424e830131d8cef0384d818f212a0f
dopiehead <ngnimitech@gmail.com>
2024-12-15 13:28:52 +0100

    new

---
+++ index-new.php
@@ -0,0 +1,531 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include 'components/links.php';     ?>
+    <link rel="stylesheet" href="assets/css/index-new.css">
+    <link rel="stylesheet" href="assets/css/overlay.css">
+    <title>Homepage</title>
+</head>
+<body class='bg-light'>
+
+<?php include 'components/marquee.php'; ?>
+ 
+<div class='nav'>
+
+     <div>
+          <img src='assets/img/logo.png'>
+         
+     </div>
+
+
+     <ul class='link-container fw-bold'>
+
+          <li><a href='service_providers.php'>Service providers</a></li>
+
+          <li><a href='charges.php'>Call out charges</a></li>
+
+          <li><a href='profile.php'>Sell a product</a> </li> 
+
+     </ul>
+
+     <div class='create-container fw-bold'>
+       
+      <span><a class='px-2 py-1  border-warning' href='login.php'>Login</a></span>
+
+      <span><a class=' bg-warning rounded rounded-pill px-2 py-1' href='sign-up.php'>Sign up</a></span>
+
+     </div>
+
+     <div class='open-nav'>
+       
+     <i class='fa fa-bars'></i>
+
+     </div>
+
+
+</div>
+
+
+     <div class='container bg-section'>
+
+        <div class='bg-text mt-4 pt-3 '>
+
+              <span class='text-warning text-sm bg-white rounded rounded-pill py-1 px-2'>READY TO HELP YOU !</span><br><br>
+
+              <h1 class='fw-bold'>The best solution for every house problem</h1><br>
+
+              <p class='text-secondary'>Our open, positive and proactive approach helps us find ways to align your work environment with the children</p><br>
+
+              <div class='button-container'>
+            
+                  <a class='rounded rounded-pill px-3 py-1 fw-bold  text-center'>Book now</a>
+
+                 <a class='rounded rounded-pill px-3 py-1 fw-bold  text-center'>Read more</a>
+        
+             </div>
+
+         </div>
+
+         <div class='bg-img'>
+
+             <img src="assets/img/hero.png" alt="efix">
+  
+         </div>
+
+
+     </div>
+
+
+<!-- body part -->
+
+
+ <div class='container about-section'>
+
+     <div class=' d-flex bg-warning text-white text-center flex-column flex-row  '>
+         <span><i class='fa fa-check fa-2x'></i></span>
+         <span>Professional Expertise</span>
+
+     </div>
+
+
+     <div class=' d-flex bg-warning text-white text-center flex-row flex-column '>
+         <span><i class='fa fa-thumbs-up fa-2x'></i></span>
+         <span>Reliable Service</span>
+
+     </div>
+
+
+     <div class=' d-flex bg-warning text-white text-center flex-row flex-column'>
+          <span><i class='fa fa-star fa-2x'></i></span>
+          <span>Cost-Effective Solutions</span>
+
+     </div>
+
+ </div>
+
+
+
+ <div class='bg-dark service-container text-center '>
+
+
+ 
+     <span class='text-white bg-secondary rounded rounded-pill px-2 py-1 mt-4 mb-3'>Services</span>  <br>
+
+     <br><br>
+
+     <h4 class='text-white fw-bold text-center '>Explore our comprehensive range<br> of Professional Services</h4><br>
+     
+     <div class='container services text-white'>
+         
+         <div>
+    
+              <a href='service-providers.php?work=information technology'><img src="assets/icons/information-technology-white.png" alt="efix"></a>
+
+              <span><a class='text-warning fw-bold' href='service-providers.php?work=information technology'>information technology</a> </span>
+
+         </div>
+
+
+         <div>
+            
+             <img src="assets/icons/electrical-white.png" alt="efix">
+
+             <span><a class='text-warning fw-bold' href='service-providers.php?work=inverter services'>inverter services</a></span>
+         
+        </div>
+
+        
+        <div>
+            
+            <img src="assets/icons/ac.png" alt="efix">
+
+            <span><a class='text-warning  fw-bold' href='service-providers.php?work=Air conditioner'>Ac</a></span>
+        
+       </div>
+
+          
+       <div>
+            
+  
+           <img src="assets/icons/mechanics-white.png" alt="efix">
+            
+            <span><a class='text-warning text-sm fw-bold' href='service-providers.php?work=mechanic'>mechanic</a></span>
+           
+    
+
+        </div>
+        
+
+        <div>
+            
+         <img src="assets/icons/make-up-white.png" alt="efix"> 
+
+         <span><a class='text-warning  fw-bold' href='service-providers.php?work=makeup artists'>Makeup Artists</a> </span>
+         
+        </div>
+         
+
+
+        <div>
+          <img src="assets/icons/vulganizer-white.png" alt="efix">
+
+          <span><a class='text-warning fw-bold' href='service-providers.php?work=vulganizer'>vulganizer</a></span>
+
+        </div>
+
+
+        
+        <div>
+          <img src="assets/icons/car-wash.png" alt="efix">
+
+          <span><a class='text-warning  fw-bold' href='service-providers.php?work=car wash'>Car Wash</a></span>
+
+        </div>
+
+
+        
+        <div>
+
+          <img src="assets/icons/pet-service.png" alt="efix">
+
+          <span><a class='text-warning fw-bold' href='service-providers.php?work=pet service'>Pet Service</a></span>
+
+        </div>
+
+
+        
+        <div>
+          <img src="assets/icons/counselling-white.png" alt="efix">
+
+          <span><a class='text-warning  fw-bold' href='service-providers.php?work=counselling'>Counseling</a></span>
+
+        </div>
+
+
+        <div>
+           
+          <img src="assets/icons/cleaning-white.png" alt="efix">
+
+          <span><a class='text-warning fw-bold' href='service-providers.php?work=cleaning'>Cleaning</a></span>
+
+        </div>
+
+
+        
+        <div>
+           
+          <img src="assets/icons/hairdressers-white.png" alt="efix">
+
+          <span><a class='text-warning  fw-bold' href='service-providers.php?work=hairdressers'>Hairdressers</a></span>
+
+        </div>
+
+
+
+        <div>
+           
+           <img src="assets/icons/wellness-white.png" alt="efix">
+
+           <span><a class='text-warning fw-bold' href='service-providers.php?work=wellness'>Wellness</a></span>
+
+         </div>
+
+     </div>
+ 
+<br><br>
+
+ </div>
+ <br>
+
+ <div class=' why-section container '>
+
+      <div>
+         
+          <img src="assets/img/why.png" alt="efix">
+
+     </div>
+
+
+
+     <div>
+
+         <small class='d-flex flex-row flex-column rounded rounded-pill text-danger px-2 fw-bold w-100'>WHY US</small><br>
+
+         <h4 class='fw-bold'>The Efix advantage: Reasons to trust our expertise</h4>
+        
+         <span class='text-sm text-secondary'>Our open, positive and proactive approach helps us find ways to align our work environment with the culture</span><br>
+         
+         <div class='mt-3'>
+
+             <span class='fw-bold rounded rounded-circle bg-dark text-white px-1'><i class='fa fa-check'></i></span>
+             <span class='text-sm fw-bold'>Monthly inspection</span>
+
+         </div>
+    
+         <div class='mt-1'>
+
+              <span class='fw-bold rounded rounded-circle bg-dark text-white px-1'><i class='fa fa-check'></i></span>
+              <span class='text-sm fw-bold'>General Repair maintenance</span>
+
+         </div>
+
+         <div class='mt-1'>
+
+              <span class='fw-bold rounded rounded-circle bg-dark text-white px-1'><i class='fa fa-check'></i></span>
+              <span class='text-sm fw-bold'>Fixing of faulty appliances</span>
+
+         </div>
+
+
+
+     </div>
+
+ </div>
+
+
+
+
+ <div class='bg-white py-5 container d-flex justify-content-between'>
+
+     <div>
+         <p class='text-warning fw-bold text-sm'>Testimonials</p>
+         <h4 class='fw-bold'>Hear what they have to always to say about<br> our professional services</h4>
+     </div>
+
+     <div class='d-flex  align-items-end'>
+           <br><br>
+         <div class='d-flex justify-content-evenly g-3'>
+              <span class='bg-warning text-white shadow  p-2 previous-button'><a><i class='fa fa-chevron-left'></i></a></span>
+              <span class='bg-warning text-white shadow p-2 next-button'><a><i class='fa fa-chevron-right'></i></a></span>
+         </div>
+
+     </div>
+ </div>
+
+ <br><br>
+
+ <div class='container mt-4 comment-section'>
+
+     <div class='py-5 px-2 comments-content d-flex align-items-center flex-row flex-column bg-white g-3'>
+
+         <span class='fa fa-quote-right text-danger fa-3x'></span>
+
+         <p class='text-sm fw-bold w-50 text-center'>Working with this handyman was a breeze, their expertise in handling tasks were impressive</p>
+          
+             <div class='d-flex align-items-center g-3'>
+                 <img src="assets/img/woman.jpg" alt="">
+
+                 <div class='d-flex flex-row flex-column'>
+                     <span class='text-sm fw-bold'>Neeyo</span>
+                     <span class='text-sm text-secondary'>Senior designer , Edirect.ng</span>
+                 </div>
+
+            </div>
+     </div>
+
+
+     <div class=' py-5 px-2  comments-content d-flex align-items-center flex-row flex-column bg-white g-3'>
+
+<span class='fa fa-quote-right text-danger fa-3x'></span>
+
+<p class='text-sm fw-bold w-50 text-center'>Working with this handyman was a breeze, their expertise in handling tasks were impressive</p>
+ 
+     <div class='d-flex align-items-center g-3'>
+          <img src="assets/img/woman.jpg" alt="">
+
+         <div class='d-flex flex-row flex-column'>
+            <span class='text-sm fw-bold'>Neeyo</span>
+            <span class='text-sm text-secondary'>Senior designer , Edirect.ng</span>
+         </div>
+
+     </div>
+</div>
+
+
+<div class=' py-5 px-2 d-flex  comments-content align-items-center flex-row flex-column bg-white g-3'>
+
+<span class='fa fa-quote-right text-danger fa-3x'></span>
+
+<p class='text-sm fw-bold w-50 text-center'>Working with this handyman was a breeze, their expertise in handling tasks were impressive</p>
+ 
+     <div class='d-flex align-items-center g-3'>
+         <img src="assets/img/woman.jpg" alt="">
+
+         <div class='d-flex flex-row flex-column'>
+            <span class='text-sm fw-bold'>Neeyo</span>
+            <span class='text-sm text-secondary'>Senior designer , Edirect.ng</span>
+         </div>
+
+     </div>
+</div>
+
+    
+
+
+ </div>
+
+ <br><br><br>
+
+ <div class='appointments bg-warning text-white py-5 d-flex align-items-center flex-row flex-column'>
+     
+   <span class='bg-light rounded rounded-pill text-capitalize text-danger text-sm px-2 py-1'>appointment</span>
+
+   <h2 class='fw-bold text-center'>Ready to get your home<br> in top shape</h2>
+
+   <span class='text-center'>Schedule your service with us today and experience quality handyman solutions</span>
+    
+   <span><a class='btn btn-light fw-bold text-warning rounded' href="">Book now <i class='fa fa-chevron-right'></i></a></span>
+
+ </div>
+
+
+ <div>
+     
+     <div>
+          
+         <span>Contact us</span>
+
+         <h4>Booking form</h4>
+
+         <p class='text-secondary'>Fill out the form below to get on your project</p>
+
+         <div class='d-flex'>
+
+               <div class='col-md-6'>
+
+                      <div class='jumbotron'>
+                       
+                          <form>
+                             <div class='d-flex'>
+                                 <div>
+                                     <label for="full name">Full name</label>
+                                     <input type="text">
+                                 </div>
+
+                                 <div>
+                                     <label for="email">Full name</label>
+                                     <input type="text">
+                                 </div>
+
+                             </div>
+
+                             <div class='d-flex'>
+                                 <div>
+                                     <label for="telephone">Telephone</label>
+                                     <input type="number">
+                                 </div>
+
+                                 <div>
+                                     <label for="date">Date</label>
+                                     <input type="date">
+                                 </div>
+
+                             </div>
+
+                             <div>
+                              
+
+                                 <div>
+                                     <label for="date">Address</label>
+                                     <textarea placeholder='Input an address'></textarea>
+                                 </div>
+
+                             </div>
+
+                             <button class='btn btn-danger'>Get a Quote</button>
+                            
+
+                           </form>
+
+
+                     </div>
+
+               </div>
+
+               <div class='col-md-6'>
+
+                   <div class='d-flex'>
+
+                          <div class='shadow'>
+
+
+                          </div>
+
+                           <div class='shadow'>
+                             
+                           <i class='fa fa-phone'></i>
+
+                           </div>
+
+
+                          <div class='shadow'>
+
+
+                          </div>
+
+
+                   </div>
+
+
+
+
+
+               </div>
+
+         </div>
+
+
+
+     </div>
+
+
+
+ </div>
+
+
+
+ 
+<!------------------------------------------btn-scroll--------------------------------------------------->
+
+<a class=" btn-down" onclick="topFunction()">&#8593;</a>
+
+
+<br><br>
+
+<!-----------------------------------------Footer--------------------------------------------------->
+
+<?php include 'components/footer.php'; ?>
+
+
+<script>
+
+var flickity = new Flickity('.comment-section', {
+  cellAlign: 'left',
+  contain: true,
+  autoPlay: true,
+});
+
+
+var prevButton = document.querySelector('.previous-button');
+var nextButton = document.querySelector('.next-button');
+
+
+
+if (prevButton && nextButton) {
+  prevButton.addEventListener('click', function() {
+    flickity.previous();
+  });
+
+  nextButton.addEventListener('click', function() {
+    flickity.next(); 
+  });
+}
+</script>
+
+
+
+    
+</body>
+</html>
